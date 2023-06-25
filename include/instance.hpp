b5c915da4470dcdc45894761d768e696d64d0216
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-06-26 00:44:39 +0530

    feat: added instance concept

---
+++ include/instance.hpp
@@ -0,0 +1,15 @@
+#include <utility>
+namespace rd {
+template <typename key_t, typename value_t> struct instance_t {
+  key_t instance_id;
+  value_t value;
+};
+
+template <typename key_t, typename value_t>
+auto to_instance(key_t key, value_t &value) -> rd::instance_t<key_t, value_t> {
+  return {
+      .instance_id = std::move(key),
+      .value = std::move(value),
+  };
+}
+} // namespace rd
