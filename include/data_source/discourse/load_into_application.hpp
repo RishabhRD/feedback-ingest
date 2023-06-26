b89ab24b61fd3e6dd1d374e3ebc461b8b7be26e6
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2023-06-26 09:44:25 +0530

    bulk code for loading

--- include/data_source/discourse/load_into_application.hpp
+++ include/data_source/discourse/load_into_application.hpp
@@ -3,21 +3,34 @@
 #include "application_state.hpp"
 #include "data_source/data_source_info_header.hpp"
 #include "data_source/data_source_operation_state.hpp"
+#include "data_source/discourse/discourse_operation_state.hpp"
+#include "tenant/tenant_registry.hpp"
 #include "timer/asio_timer.hpp"
 namespace rd {
 namespace discourse {
 
-inline auto load_into_application(
-    int tenant_id,
-    rd::data_source_info_header_t<discourse_info_t> discourse_info,
-    rd::application_state_t &state) {
-  auto const source_id =
-      state.data_source_info_registry.register_value(discourse_info);
-  rd::discourse::discourse_operation_state_t op_state(
-      source_id, tenant_id, discourse_info.source_info,
-      rd::asio_timer::execute_every_t{});
-  state.data_source_operation_state_registry.register_value(
-      std::move(op_state));
+using info_t = rd::data_source_info_header_t<rd::discourse::discourse_info_t>;
+using tenant_id_t = tenant_registry_t::key_t;
+
+struct data_load_entry_t {
+  info_t source_info_with_header;
+  std::vector<int> subscribing_tenants;
+};
+
+inline auto load_into_application(std::vector<data_load_entry_t> data_to_load,
+                                  application_state_t &state) {
+  for (auto const &[source_info_with_header, tenants] : data_to_load) {
+    auto const source_id =
+        state.data_source_info_registry.register_value(source_info_with_header);
+    for (auto tenant_id : tenants) {
+      auto op_state = rd::discourse::discourse_operation_state_t{
+          source_id, tenant_id, source_info_with_header.source_info,
+          rd::asio_timer::execute_every_t{}};
+
+      state.data_source_operation_state_registry.register_value(
+          std::move(op_state));
+    }
+  }
 }
 } // namespace discourse
 } // namespace rd
--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,7 @@
 //
 // using namespace boost::beast;
 
-int main() {
-  rd::application_state_t state;
+void load_discourse_tasks(rd::application_state_t &state) {
   rd::tenant_info_t t1{
       .owner_email = "enterpret@enterpret.com",
       .organisation = "enterpret",
@@ -51,9 +50,16 @@ int main() {
               .topic_id = 31963,
           },
   };
-  rd::discourse::load_into_application(t1_id, s1, state);
-  rd::discourse::load_into_application(t1_id, s2, state);
-  rd::discourse::load_into_application(t2_id, s1, state);
-  rd::discourse::load_into_application(t2_id, s2, state);
+
+  rd::discourse::data_load_entry_t e1{s1, std::vector({t1_id, t2_id})};
+  rd::discourse::data_load_entry_t e2{s2, std::vector({t1_id, t2_id})};
+
+  std::vector<rd::discourse::data_load_entry_t> entries_to_load{e1, e2};
+  rd::discourse::load_into_application(entries_to_load, state);
+}
+
+int main() {
+  rd::application_state_t state;
+  load_discourse_tasks(state);
   state.data_source_operation_state_registry.ctx.run();
 }
